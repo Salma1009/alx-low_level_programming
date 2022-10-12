f360ed13b1723d20a45368923834c47be2281438
SAL <salma.m1009@yahoo.com>
2022-10-12 13:17:59 -0700

    function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,14 +3,10 @@
 
 #include <stdlib.h>
 
-/*
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0E-function_pointers directory.
- */
-
-
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
+
 #endif
+
